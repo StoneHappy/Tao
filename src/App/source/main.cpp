27f29c937036db1f9c8563463b180e29348b62e5
StoneHappy <18017158729@163.com>
2022-08-26 15:10:31 +0800

    🧪 test: test multi thread

--- src/App/source/main.cpp
+++ src/App/source/main.cpp
@@ -1,8 +1,20 @@
+#include <chrono>
 #include <iostream>
 #include <FrontEnd/index.h>
+#include <cpp-utilities/thread_pool.h>
+#include <thread>
 int main(int argc, char *argv[])
 {
-    PublicSingleton<Tao::Window>::getInstance().Setup();
-    PublicSingleton<Tao::Window>::getInstance().Run();
-    PublicSingleton<Tao::Window>::getInstance().Shutdown();
+    thread_pool pool;
+    pool.add_worker([](){
+        PublicSingleton<Tao::Window>::getInstance().Setup();
+        PublicSingleton<Tao::Window>::getInstance().Run();
+        PublicSingleton<Tao::Window>::getInstance().Shutdown();
+    });
+    pool.add_worker([](){
+        for (int i=0; i < 5; i++) {
+            std::this_thread::sleep_for(std::chrono::seconds(5));
+            std::cout << i << std::endl;
+        }
+    });
 }
