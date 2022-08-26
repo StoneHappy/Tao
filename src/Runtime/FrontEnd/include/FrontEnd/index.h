d1e5394c77354a63582fb08eea815a2c6de315f5
ImGili <18017158729@163.com>
2022-08-26 13:15:00 +0800

    ✨ feat: init window

--- src/Runtime/FrontEnd/include/FrontEnd/index.h
+++ src/Runtime/FrontEnd/include/FrontEnd/index.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <cpp-utilities/singleton.hpp>
 #include <cpp-utilities/dll.h>
+#include <GLFW/glfw3.h>
 namespace Tao
 {
     class CPP_UTIL_API Window : public PublicSingleton<Window>
@@ -13,5 +14,6 @@ namespace Tao
 
         void Shutdown();
     private:
+        GLFWwindow* m_Window;
     };
 }
--- src/Runtime/FrontEnd/src/Window.cpp
+++ src/Runtime/FrontEnd/src/Window.cpp
@@ -1,11 +1,18 @@
 #include "FrontEnd/index.h"
+#include "GLFW/glfw3.h"
 #include <iostream>
 namespace Tao
 {
     void Window::Setup(){
+        glfwInit();
+        m_Window = glfwCreateWindow(200, 300, "Tao", nullptr, nullptr);
         std::cout << "Setup..." << std::endl;
     }
     void Window::Run(){
+        while (!glfwWindowShouldClose(m_Window))
+        {
+            glfwPollEvents();
+        }
         std::cout << "Run..." << std::endl;
     }
     void Window::Shutdown(){
