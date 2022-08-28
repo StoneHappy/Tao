388250408dacc8eb84509f12e57eb90f4c4560d0
StoneHappy <18017158729@163.com>
2022-08-28 21:57:03 +0800

    📃 docs: add comments

--- src/Runtime/Core/include/Tao/Core/index.h
+++ src/Runtime/Core/include/Tao/Core/index.h
@@ -5,9 +5,19 @@
 namespace Tao
 {
     namespace Core {
+        /**
+         * @brief Tao Core Application config Tao app by cmdline.
+         * 
+         */
         class CPP_UTIL_API Application
         {
         public:
+            /**
+             * @brief Tao core application
+             * @details 
+                1. -s or --scriptpath Specify the Lua script file directory, and Tao will automatically parse the main.lua file in the directory. <br> 
+                2. -? or --help Show all argurements.
+             */
             Application(int argc, char** argv);
         private:
             cmdline::parser cmdline_parser;
